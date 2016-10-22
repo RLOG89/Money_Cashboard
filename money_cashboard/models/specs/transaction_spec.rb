26b7b81d104c4a1780d6b29ee76d41978e8bead0
RLOG89 <ross.loggie@gmail.com>
2016-10-22 11:56:26 +0100

    tested transaction_has_date

--- money_cashboard/models/specs/transaction_spec.rb
+++ money_cashboard/models/specs/transaction_spec.rb
@@ -1,6 +1,20 @@
-require_relative('minitest/autorun')
-require_relative('minitest/rg')
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../transaction')
 
 class TestTransaction < MiniTest::Test
 
+  def setup
+    options = {
+      'date' => 22/10/2016,
+      'merchant' => "Tesco",
+      'description' => "Weekly food shop",
+      'amount' => 50
+    }
+    @transaction = Transaction.new( options )
+  end
+
+  def test_transaction_has_date
+    assert_equal(22/10/2016, @transaction.date)
+  end
 end
