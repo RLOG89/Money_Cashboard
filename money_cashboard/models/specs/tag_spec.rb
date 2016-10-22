52411bf971f98b479f9fb686a4033d11413a5794
RLOG89 <ross.loggie@gmail.com>
2016-10-22 11:44:32 +0100

    tested tag_has_name

--- money_cashboard/models/specs/tag_spec.rb
+++ money_cashboard/models/specs/tag_spec.rb
@@ -1,6 +1,19 @@
-require_relative('minitest/autorun')
-require_relative('minitest/rg')
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../tag')
 
 class TestTransaction < MiniTest::Test
+
+  def setup
+    options = {
+      'name' => "Food",
+      'colour' => "Red"
+    }
+    @tag = Tag.new( options )
+  end
+
+  def test_tag_has_name
+    assert_equal("Food", @tag.name)
+  end
 
 end
