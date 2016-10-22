c7dd7b0675e9d52947ba71c88c813c1eae7b4de9
RLOG89 <ross.loggie@gmail.com>
2016-10-22 16:28:49 +0100

    added create root to transaction_controller

--- money_cashboard/controllers/transaction_controller.rb
+++ money_cashboard/controllers/transaction_controller.rb
@@ -1,6 +1,6 @@
 require_relative('../models/transaction')
 
-get '/transactions' do
+get '/transactions/?' do
   @transactions = Transaction.all
   erb(:'transactions/index')
 end
@@ -8,4 +8,10 @@ end
 get '/transactions/new' do
   @transactions = Transaction.all
   erb(:'transactions/new')
+end
+
+post '/transactions/?' do
+  @transaction = Transaction.new( params )
+  @transaction.save
+  redirect to('/transactions')
 end
