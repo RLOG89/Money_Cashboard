9790bebe5a4dff43a9dc9783cb17f06865749491
RLOG89 <ross.loggie@gmail.com>
2016-10-23 13:43:56 +0100

    changed console to seeds and populated some more data

--- money_cashboard/db/seeds.rb
+++ money_cashboard/db/seeds.rb
@@ -4,8 +4,10 @@ require_relative('./models/transaction')
 
 tag1 = Tag.new({'name' => "Food", 'colour' => "blue"})
 tag1.save
+tag2 = Tag.new({'name' => "Pub", 'colour' => "red"})
 
 transaction1 = Transaction.new({'date' => '2016/10/22', 'merchant' => "Sainsburys", 'description' => "Weekly shop", 'amount' => 43.50, 'tag_id' => tag1.id})
 transaction1.save
 
-
+transaction2 = Transaction.new({'date' => '2016/10/24', 'merchant' => "The Chanter", 'description' => "After work pints", 'amount' => 21.55, 'tag_id' => tag2.id})
+transaction2.save
