f106f4fa02b607e257b685fdc0c7b71c9ce60a47
RLOG89 <ross.loggie@gmail.com>
2016-10-26 16:16:24 +0100

    added instance variables to controller

--- money_cashboard/controller.rb
+++ money_cashboard/controller.rb
@@ -5,5 +5,7 @@ require_relative('./controllers/tag_controller')
 require_relative('./controllers/merchant_controller')
 
 get '/' do
+  @tags = Tag.all
+  @transactions = Transaction.all
   erb( :home )
 end
