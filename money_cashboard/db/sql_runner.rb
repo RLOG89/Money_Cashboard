188e89ccfffe45859b21b35cf315800adcfd4dd4
RLOG89 <ross.loggie@gmail.com>
2016-11-04 16:11:08 +0000

    altered sql runner

--- money_cashboard/db/sql_runner.rb
+++ money_cashboard/db/sql_runner.rb
@@ -1,10 +1,19 @@
 require 'pg'
+require 'uri'
 
 class SqlRunner
 
   def self.run( sql )
+
     begin
-      db = PG.connect( {dbname: 'cashboard', host: 'localhost'} )
+      
+      if ENV['DATABASE_URL'] == nil
+        db = PG.connect( {dbname: 'cashboard', host: 'localhost'} )
+      else
+        uri = URI.prase(ENV['DATABASE_URL'])
+        db = PG.connect( dbname: uri.path[1..-1], host: uri.host, user: uri.user, password: uri.password)
+      end
+
       result = db.exec( sql )
     ensure
       db.close
